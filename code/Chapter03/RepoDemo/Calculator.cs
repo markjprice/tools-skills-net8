54a46df4e274d17f70d08676b7ce696540b95f2a
Mark J Price <markjprice@gmail.com>
2024-02-08 15:54:50 +0000

    Add code solution for Ch3

---
+++ code/Chapter03/RepoDemo/Calculator.cs
@@ -0,0 +1,7 @@
+public static class Calculator
+{
+  public static int Add(int a, int b)
+  {
+    return a + b;
+  }
+}
---
+++ code/Chapter03/RepoDemo/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Git!");
+Console.WriteLine(Calculator.Add(2, 3));
