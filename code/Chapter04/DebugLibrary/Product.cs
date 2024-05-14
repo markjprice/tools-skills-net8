7a36d2c8deadc294b7acd1b02eac486c752699ef
Mark J Price <markjprice@gmail.com>
2024-05-14 08:46:05 +0100

    Updated code solutions for Ch4 and 5 revisions

---
+++ code/Chapter04/DebugLibrary/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Northwind.EntityModels;
+
+public class Product
+{
+  public int ProductId { get; set; }
+  public string ProductName { get; set; } = null!;
+  public int? SupplierId { get; set; }
+  public int? CategoryId { get; set; }
+  public string? QuantityPerUnit { get; set; }
+  public decimal? UnitPrice { get; set; }
+  public short? UnitsInStock { get; set; }
+  public short? UnitsOnOrder { get; set; }
+  public short? ReorderLevel { get; set; }
+  public bool Discontinued { get; set; }
+}
---
+++ code/Chapter04/MemoryApp/Program.cs
@@ -0,0 +1,14 @@
+﻿List<Person> people = new();
+
+for (int i = 0; i < 10_000; i++)
+{
+  people.Add(new Person { Name = $"Person {i}", Age = i });
+}
+
+WriteLine($"Finished creating {people.Count} people.");
+
+class Person
+{
+  public string? Name { get; set; }
+  public int Age { get; set; }
+}
