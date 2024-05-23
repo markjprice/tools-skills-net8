f0c7803a7695db6c0467b4f20f22d73a517c4577
Mark J Price <markjprice@gmail.com>
2024-05-23 07:48:52 +0100

    Updated for Aspire GA

--- code/Chapter16/AspireNew/AspireNew.AppHost/Program.cs
+++ code/Chapter16/AspireNew/AspireNew.AppHost/Program.cs
@@ -4,7 +4,7 @@ builder.AddPostgres("postgres")
   .WithPgAdmin()
   .AddDatabase("cms");
 
-builder.AddSqlServer("sqlserver"/*, port: 1433*/)
+builder.AddSqlServer("sqlserver")
   .AddDatabase("northwind");
 
 builder.AddProject<Projects.Northwind_WebApi>("northwind-webapi");
