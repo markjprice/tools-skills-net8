e1338c1e9b61156435d00a94e7bc667f302f7994
Mark J Price <markjprice@gmail.com>
2024-06-05 12:16:39 +0100

    Update code solutions

---
+++ code/Chapter09/ChatApp/Program.cs
@@ -0,0 +1,75 @@
+﻿// Disable experimental warnings.
+#pragma warning disable SKEXP0003, SKEXP0011, SKEXP0028, SKEXP0052, SKEXP0055
+
+using Microsoft.SemanticKernel; // To use Kernel.
+
+// To use ChatHistory and so on.
+using Microsoft.SemanticKernel.ChatCompletion;
+
+// To use OpenAIPromptExecutionSettings.
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+using Microsoft.SemanticKernel.Memory; // To use ISemanticTextMemory.
+using System.Text; // To use StringBuilder.
+
+Settings? settings = GetSettings();
+if (settings is null)
+{
+  WriteLine("Settings not found or not valid. Exiting the app.");
+  return; // Exit the app.
+}
+
+Kernel kernel = GetKernel(settings);
+
+// $question will be defined as an argument.
+//KernelFunction function = kernel.CreateFunctionFromPrompt("""
+//  Author biography: {{ authorInformation.getAuthorBiography }}.
+//  {{ $question }}
+//  """);
+
+//KernelArguments arguments = new();
+
+ConsoleKey key = ConsoleKey.A;
+
+IChatCompletionService completion =
+  kernel.GetRequiredService<IChatCompletionService>();
+
+ChatHistory history = new(systemMessage: "You are an AI assistant based on Mark J Price's knowledge, skills, and experience.");
+
+OpenAIPromptExecutionSettings options = new()
+{ ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
+
+// To help implement async streaming output.
+StringBuilder builder = new();
+
+while (key is not ConsoleKey.X)
+{
+  Write("Enter your question: ");
+  string question = ReadLine()!;
+
+  // WriteLine(await kernel.InvokePromptAsync(question));
+
+  // arguments["question"] = question;
+  // Call a single function.
+  // WriteLine(await function.InvokeAsync(kernel, arguments));
+
+  history.AddUserMessage(question);
+
+  //ChatMessageContent answer = await 
+  //  completion.GetChatMessageContentAsync(history);
+
+  builder.Clear();
+  await foreach (StreamingChatMessageContent message
+    in completion.GetStreamingChatMessageContentsAsync(
+      history, options, kernel))
+  {
+    Write(message.Content);
+    builder.Append(message.Content);
+  }
+
+  history.AddAssistantMessage(builder.ToString());
+
+  WriteLine();
+  WriteLine("Press X to exit or any other key to ask another question.");
+  key = ReadKey(intercept: true).Key;
+}
---
+++ code/Chapter09/LlamaApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using OllamaSharp; // To use OllamaApiClient.
+using OllamaSharp.Models; // To use Model.
+using Spectre.Console; // To use Table.
+using System.Diagnostics; // To use Stopwatch.
+
+// Default port for the Ollama API is 11434.
+string port = "11434";
+Uri uri = new($"http://localhost:{port}");
+
+OllamaApiClient ollama = new(uri);
+
+Table table = new();
+table.AddColumn("Name");
+table.AddColumn("Size");
+
+// Get the list of models.
+IEnumerable<Model> models = await ollama.ListLocalModels();
+foreach (Model model in models)
+{
+  table.AddRow(model.Name, model.Size.ToString("N0"));
+}
+
+AnsiConsole.Write(table);
+
+string modelName = "llama3:latest";
+
+WriteLine();
+WriteLine($"Selected model: {modelName}");
+ollama.SelectedModel = modelName;
+
+Write("Please enter your prompt: ");
+string? prompt = ReadLine();
+if (string.IsNullOrWhiteSpace(prompt))
+{
+  WriteLine("Prompt is required. Exiting the app.");
+  return;
+} 
+
+Stopwatch timer = Stopwatch.StartNew();
+
+ConversationContext context = new([]);
+context = await ollama.StreamCompletion(
+  prompt, context, stream => Write(stream.Response));
+
+timer.Stop();
+
+WriteLine();
+WriteLine();
+WriteLine($"Elapsed time: {timer.ElapsedMilliseconds:N0} ms");
